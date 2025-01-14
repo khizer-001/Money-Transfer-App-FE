7b9f24f2cd391b089e73cd56ac9eff161e3aeddc
khizer-001 <khizzerkhan572@gmail.com>
2025-01-15 00:49:18 +0500

    nav component

--- src/pages/Transfer.jsx
+++ src/pages/Transfer.jsx
@@ -1,10 +1,12 @@
 import 'react'
 
 import TransferForm from "../components/TransferForm"
+import Navbar from '../components/Navbar'
 
 const Transfer = () => {
   return (
     <div>
+      <Navbar />
         <TransferForm />
     </div>
   )
